59a60925720f6900d02feb71da77852a8cccc7bb
Gerard Verdu Castelar <gerard.verdu@estudiant.enti.cat>
2026-04-26 14:56:07 +0200

    MainMenu scene, GameManager script, show credits working

---
+++ Assets/HookPlatformer2D/Scripts/GameManager.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else Destroy(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
